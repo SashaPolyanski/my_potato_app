1a5ab597965d614bd0d453b1c891060dd6ec98c8
SashaPolyanski <Sashapolyanski@gmail.com>
2023-11-09 11:41:03 +0300

    added actions to git actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,15 +3,21 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export const buildPlugins = (html: string, isDev: boolean): webpack.WebpackPluginInstance[] => [
-  new webpack.ProgressPlugin(),
-  new BundleAnalyzerPlugin(),
-  new HtmlWebpackPlugin({ template: html }),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css'
-  }),
-  new webpack.DefinePlugin({
-    _IS_DEV_: JSON.stringify(isDev)
-  })
-]
+export const buildPlugins = (html: string, isDev: boolean): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new webpack.ProgressPlugin(),
+
+    new HtmlWebpackPlugin({ template: html }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new webpack.DefinePlugin({
+      _IS_DEV_: JSON.stringify(isDev)
+    })
+  ]
+  if (isDev) {
+    plugins.push(new BundleAnalyzerPlugin())
+  }
+  return plugins
+}
